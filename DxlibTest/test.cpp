3e10c7a56cf77b071f9fc576af659b6d6f6b1fa8
satochibi <satochibi0120@gmail.com>
2016-10-02 01:41:40 +0900

    1章の終わりまで http://dixq.net/g/01_09.html

--- DxlibTest/test.cpp
+++ DxlibTest/test.cpp
@@ -1,42 +1,28 @@
 #include "DxLib.h"
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-	ChangeWindowMode(TRUE);//ウィンドウモードに設定
-	DxLib_Init();//DXライブラリ初期化処理
-	SetDrawScreen(DX_SCREEN_BACK); //描画先を裏画面に設定
+	ChangeWindowMode(TRUE), DxLib_Init(), SetDrawScreen(DX_SCREEN_BACK);//ウィンドウモード変更と初期化と裏画面設定
 
 	int GrHandle = LoadGraph("img/profile3D.jpg");//メモリへロード
 
 
 	int x = 50;
 
-	while (TRUE)
+	while (ScreenFlip()==0 && ProcessMessage()==0 && ClearDrawScreen() == 0)
 	{
-		
-		//	ウインドウのメッセージを処理
-		//もしxが押されたらループを抜ける
-		if (ProcessMessage() != 0)
-		{
-			break;
-		}
-		
-
-
 		//Escが押されたらループを抜ける
 		if (CheckHitKey(KEY_INPUT_ESCAPE) == 1)
 		{
 			break;
 		}
 
-
-		x++;
-
-
-
-		ClearDrawScreen();// 画面を消す
+		//裏絵画
 		DrawGraph(x, 50, GrHandle, TRUE);//メモリにロードしたやつを表示
-		ScreenFlip(); //裏画面を表画面に反映
+		DrawGraph(x/2, 100, GrHandle, TRUE);//メモリにロードしたやつを表示
+		DrawGraph(x/4, 150, GrHandle, TRUE);//メモリにロードしたやつを表示
 
+		//計算
+		x++;
 	}
 
 	//WaitKey();//キー入力待ち
