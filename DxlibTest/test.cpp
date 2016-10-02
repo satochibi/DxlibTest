e2c601c317d098bacd86af1a2c708e65c3c0208f
satochibi <satochibi0120@gmail.com>
2016-10-02 09:47:58 +0900

    2-1章まで http://dixq.net/g/02_01.html

--- DxlibTest/test.cpp
+++ DxlibTest/test.cpp
@@ -3,7 +3,8 @@
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	ChangeWindowMode(TRUE), DxLib_Init(), SetDrawScreen(DX_SCREEN_BACK);//ウィンドウモード変更と初期化と裏画面設定
 
-	int GrHandle = LoadGraph("img/profile3D.jpg");//メモリへロード
+	int GrHandle00 = LoadGraph("img/01.png");//メモリへロード
+	int GrHandle01 = LoadGraph("img/02.png");
 
 
 	int x = 50;
@@ -17,9 +18,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		}
 
 		//裏絵画
-		DrawGraph(x, 50, GrHandle, TRUE);//メモリにロードしたやつを表示
-		DrawGraph(x/2, 100, GrHandle, TRUE);//メモリにロードしたやつを表示
-		DrawGraph(x/4, 150, GrHandle, TRUE);//メモリにロードしたやつを表示
+		DrawGraph(x/2, 100, GrHandle00, TRUE);//メモリにロードしたやつを表示
+		DrawGraph(x, 50, GrHandle01, TRUE);//メモリにロードしたやつを表示
+
 
 		//計算
 		x++;
