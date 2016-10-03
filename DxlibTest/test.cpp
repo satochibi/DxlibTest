55f55bb579853ab52163c21ded370db4d23ddf13
satochibi <satochibi0120@gmail.com>
2016-10-03 11:54:33 +0900

    2-7章まで http://dixq.net/g/02_07.html

--- DxlibTest/test.cpp
+++ DxlibTest/test.cpp
@@ -8,7 +8,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	int GrHandle00 = LoadGraph("img/01.png");//メモリへロード
 	int GrHandle01 = LoadGraph("img/02.png");
 
+	int SHandle = LoadSoundMem("snd/00.wav");
 
+	int flame = 0;
 	int x = 50;
 	int y = 10;
 
@@ -40,12 +42,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		DrawFormatString(x, y, Col, "Point[%d, %d]", x, y);
 
 		DrawFormatString(100, 100, Col, "%d", GetRand(4));//乱数 seedは自動で与えられるので考慮しなくてよい
+		DrawFormatString(100, 150, Col, "flame: %d time:%.3f", flame, flame/60.0f);//乱数 seedは自動で与えられるので考慮しなくてよい
 
 
+
+		if (flame % (60*5) == 0)
+		{
+			PlaySoundMem(SHandle, DX_PLAYTYPE_BACK);
+		}
 
 		//計算
 		x++;
 		y++;
+		flame++;
 	}
 
 	//WaitKey();//キー入力待ち
